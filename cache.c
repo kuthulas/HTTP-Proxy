07ebaeefe8d883fe370913f818ace998cc06f915
jnaneshm <jnaneshm@users.noreply.github.com>
2014-10-10 11:19:05 -0500

    Node Insert & Delete in List complete

--- cache.c
+++ cache.c
@@ -5,9 +5,10 @@
 enum query {INSERT,DELETE,HITMISS};
 
 typedef struct request{
-	char * address;
-	char * resource;
+	char address[50];
+	char resource[50];
 } request;
+	request r;
 
 typedef struct page{
 	request req;
@@ -23,9 +24,11 @@ int cachemanager(enum query q, request r){
 	page *temp1,*temp2;
 	switch(q){
 		case INSERT:
+		printf("malloc Insert ");
 		here = (page *)malloc(sizeof(page));
-		here->req.address = r.address;
-		here->req.resource = r.resource;
+		strcpy(here->req.address,r.address);
+		strcpy(here->req.resource,r.resource);
+		if(head==NULL){ head=here; return 0;}
 		head->prev = here;
 		here->next = head;
 		here->prev = NULL;
@@ -35,16 +38,14 @@ int cachemanager(enum query q, request r){
 		here = head;
 		while(here!=NULL){
 			printf("A\n");
-			if(here->req.address==r.address && here->req.resource==r.resource){
+			if(!strcmp(here->req.address,r.address) && !strcmp(here->req.resource,r.resource)){
 				printf("B\n");
 				temp1 = here->prev;
 				temp2 = here->next;
-				temp1->next = temp2;
-				temp2->prev = temp1;
+				if(temp1!=NULL)temp1->next = temp2;
+				if(temp2!=NULL)temp2->prev = temp1;
 				printf("A\n");
 				free(here);
-				free(temp1);
-				free(temp2);
 				return 0;
 			}
 			here = here->next;
@@ -69,26 +70,35 @@ int cachemanager(enum query q, request r){
 }
 
 int main(){
-	request r;
 	int i;
-	char *item = "kumaran";
-	for(i=0;i<7;i++){
-		r.address = item+i;
+	char item[10] = "kumaran";
+	for(i=0;i<5;i++){
+		printf("\nEnter info to INSERT in slot %d:",i);
+		scanf("%s",r.address);
+		//strcpy(r.address,item);
 		cachemanager(INSERT, r);
 	}
-
+	printf("Insrt done");
 	page *here = head;
 	while(here!=NULL){
-		printf("%s\n", here->req.address);
+		printf("\n%s\n", here->req.address);
 		here = here->next;
+		if(here!=NULL) printf("Continue\n");
 	}
-
-	r.address = item+4;
+	printf("Traverse done");
+	
+	printf("\nEnter info to delete:");
+	scanf("%s",r.address);
+	//strcpy(r.address,item);
+	printf("\nDel start\n");
 	cachemanager(DELETE,r);
+	printf("\nDel done\n");
 
 	here = head;
 	while(here!=NULL){
-		printf("%s\n", here->req.address);
+		printf("\n%s\n", here->req.address);
 		here = here->next;
 	}
+	printf("END\n");
+	return 0;
 }
